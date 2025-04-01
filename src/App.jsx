1a5d97b69901ad01af0cd34f611f4cbbae9a219e
Saroj Tamang <sarojtmg992@gmail.com>
2025-04-01 12:46:03 +0100

    cube

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas, useFrame } from "@react-three/fiber";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function Cube() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <mesh>
+      //render any 3d shapes
+      <boxGeometry args={[4,4,6]}/> //cube
+      <meshStandardMaterial/> //cube colour
+    </mesh>
+  );
 }
 
-export default App
+export default function R3fDemo() {
+  return (
+    <Canvas>
+      <Cube />
+    </Canvas>
+  );
+}
+
+
+
+// https://codesandbox.io/p/sandbox/react-three-fiber-practice02-box-rotation-by-mouse-move-d9mb5w 
+// codes that rotates with mouse, it will rotate without click but its somethins

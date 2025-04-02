aec85e4bdb0f56f23f5cbed3b3f4786ef9e88d0a
Saroj Tamang <sarojtmg992@gmail.com>
2025-04-02 11:26:05 +0100

    light

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Canvas, useFrame } from "@react-three/fiber";
-
+// https://www.youtube.com/watch?v=vTfMjI4rVSI&t=1998s&ab_channel=rithmic
 function Cube() {
   return (
     <mesh>
       //render any 3d shapes
+      <directionalLight position={[0,0,2]}/> //manipulate the direction of the incoming light
       <boxGeometry args={[4,4,6]}/> //cube
-      <meshStandardMaterial/> //cube colour
+      <meshStandardMaterial color={'red'}/> //cube colour
     </mesh>
   );
 }
@@ -22,3 +23,4 @@ export default function R3fDemo() {
 
 // https://codesandbox.io/p/sandbox/react-three-fiber-practice02-box-rotation-by-mouse-move-d9mb5w 
 // codes that rotates with mouse, it will rotate without click but its somethins
+

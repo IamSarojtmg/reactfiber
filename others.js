016d9b7c4a2b42aa59744d7e01698fe3645a1e2e
Saroj Tamang <sarojtmg992@gmail.com>
2025-04-13 12:51:49 +0100

    quaternion

---
+++ others.js
@@ -0,0 +1,154 @@
+// // import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+// // import { TextureLoader } from "three";
+// // import { useRef, useState } from "react";
+// // import { TrackballControls } from "@react-three/drei";
+
+
+// // const pos = [0, 0, 7]
+// // const defaultPos = [3,3,6]
+// // function Cube({ position, size, targetRotation }) {
+// //   const meshRef = useRef();
+
+// //   const musicTexture = useLoader(TextureLoader, "/image/2.jpg");
+// //   const footballTexture = useLoader(TextureLoader, "/image/1.jpeg");
+// //   const threeTexture = useLoader(TextureLoader, "/image/3.jpg");
+// //   const fourTexture = useLoader(TextureLoader, "/image/4.jpg");
+// //   const fiveTexture = useLoader(TextureLoader, "/image/5.jpg");
+// //   const sixTexture = useLoader(TextureLoader, "/image/6.jpg");
+
+// //   useFrame(() => {
+// //     if (!meshRef.current) return;
+
+// //     meshRef.current.rotation.y +=
+// //       (targetRotation.y - meshRef.current.rotation.y) * 0.1;
+// //     meshRef.current.rotation.x +=
+// //       (targetRotation.x - meshRef.current.rotation.x) * 0.1;
+// //   });
+
+// //   return (
+// //     <mesh position={position} ref={meshRef}>
+// //       <boxGeometry args={size} /> /
+// //       <meshStandardMaterial map={musicTexture} attach="material-0" />
+// //       <meshStandardMaterial map={footballTexture} attach="material-1" />
+// //       <meshStandardMaterial map={threeTexture} attach="material-2" />
+// //       <meshStandardMaterial map={fourTexture} attach="material-3" />
+// //       <meshStandardMaterial map={fiveTexture} attach="material-4" />
+// //       <meshStandardMaterial map={sixTexture} attach="material-5" />
+// //     </mesh>
+// //   );
+// // }
+
+// // export default function R3fDemo() {
+// //   const [targetRotation, setTargetRotation] = useState({ x: 0, y: 0 });
+
+// //   const handleMusicClick = () => {
+// //     setTargetRotation({ x: 0, y: -Math.PI / 2 });
+// //   };
+// //   const handleSportsClick = () => {
+// //     setTargetRotation({ x: 0, y: Math.PI / 2 });
+// //   };
+
+// //   return (
+// //     <>
+// //       <button className="d" onClick={handleMusicClick}>
+// //         Music
+// //       </button>
+// //       <button className="d" onClick={handleSportsClick}>
+// //         Sports
+// //       </button>
+
+// //       <Canvas camera={{ position: defaultPos, fov: 50 }}>
+// //         <ambientLight intensity={1} />
+// //         <directionalLight position={[-2, 2, 3]} />
+// //         <Cube
+// //           position={[0, 0, 0]}
+// //           size={[2.5, 2.5, 2.5]}
+// //           targetRotation={targetRotation}
+// //         />
+// //         <TrackballControls rotateSpeed={3} noZoom={true} noPan={true} />
+// //       </Canvas>
+// //     </>
+// //   );
+// // }
+
+// //usefram rotates the cube, meaning i will have to do something there, either find a way to change the camera to point to the specific side of cube or rotate the cube to the side where you want to camera to see it.
+// //I think i will have to use usethree camera 
+// import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+// import { TextureLoader } from "three";
+// import { useRef, useState } from "react";
+// import { TrackballControls } from "@react-three/drei";
+
+// const defaultPos = [3, 3, 7];
+
+// function Cube({ position, size, targetRotation }) {
+//   const meshRef = useRef();
+//   const textures = useLoader(TextureLoader, [
+//     "/image/2.jpg",
+//     "/image/1.jpeg",
+//     "/image/3.jpg",
+//     "/image/4.jpg",
+//     "/image/5.jpg",
+//     "/image/6.jpg",
+//   ]);
+
+//   useFrame(() => {
+//     if (!meshRef.current) return;
+//     meshRef.current.rotation.y +=
+//       (targetRotation.y - meshRef.current.rotation.y) * 0.1;
+//     meshRef.current.rotation.x +=
+//       (targetRotation.x - meshRef.current.rotation.x) * 0.1;
+//       meshRef.current.rotation.z +=
+//       (targetRotation.z - meshRef.current.rotation.z) * 0.1;
+
+
+
+
+//   });
+
+//   return (
+//     <mesh position={position} ref={meshRef}>
+//       <boxGeometry args={size} />
+//       {textures.map((texture, index) => (
+//         <meshStandardMaterial
+//           key={index}
+//           map={texture}
+//           attach={`material-${index}`}
+//         />
+//       ))}
+//     </mesh>
+//   );
+// }
+
+// export default function R3fDemo() {
+//   const [targetRotation, setTargetRotation] = useState({ x: 0, y: 0, z: 0 });
+
+//   const handleMusicClick = () => {
+//     setTargetRotation({ x: 0, y: -Math.PI / 2, z:0 });
+//   };
+
+//   const handleSportsClick = () => {
+//     setTargetRotation({ x: 0, y: Math.PI / 2,z:0 });
+//   };
+
+//   return (
+//     <>
+//       <button className="d" onMouseEnter={handleMusicClick}>
+//         Music
+//       </button>
+//       <button className="d" onMouseEnter={handleSportsClick}>
+//         Sports
+//       </button>
+
+//       <Canvas camera={{ position: defaultPos, fov: 50 }}>
+//         <ambientLight intensity={1} />
+//         <directionalLight position={[-2, 2, 3]} />
+//         <Cube
+//           position={[0, 0, 0]}
+//           size={[2.5, 2.5, 2.5]}
+//           targetRotation={targetRotation}
+//         />
+//         <TrackballControls rotateSpeed={1} noZoom noPan makeDefault />
+//       </Canvas>
+//     </>
+//   );
+// }
--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,17 @@
-// import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-// import { TextureLoader } from "three";
-// import { useRef, useState } from "react";
-// import { TrackballControls } from "@react-three/drei";
-
-
-// const pos = [0, 0, 7]
-// const defaultPos = [3,3,6]
-// function Cube({ position, size, targetRotation }) {
-//   const meshRef = useRef();
-
-//   const musicTexture = useLoader(TextureLoader, "/image/2.jpg");
-//   const footballTexture = useLoader(TextureLoader, "/image/1.jpeg");
-//   const threeTexture = useLoader(TextureLoader, "/image/3.jpg");
-//   const fourTexture = useLoader(TextureLoader, "/image/4.jpg");
-//   const fiveTexture = useLoader(TextureLoader, "/image/5.jpg");
-//   const sixTexture = useLoader(TextureLoader, "/image/6.jpg");
-
-//   useFrame(() => {
-//     if (!meshRef.current) return;
-
-//     meshRef.current.rotation.y +=
-//       (targetRotation.y - meshRef.current.rotation.y) * 0.1;
-//     meshRef.current.rotation.x +=
-//       (targetRotation.x - meshRef.current.rotation.x) * 0.1;
-//   });
-
-//   return (
-//     <mesh position={position} ref={meshRef}>
-//       <boxGeometry args={size} /> /
-//       <meshStandardMaterial map={musicTexture} attach="material-0" />
-//       <meshStandardMaterial map={footballTexture} attach="material-1" />
-//       <meshStandardMaterial map={threeTexture} attach="material-2" />
-//       <meshStandardMaterial map={fourTexture} attach="material-3" />
-//       <meshStandardMaterial map={fiveTexture} attach="material-4" />
-//       <meshStandardMaterial map={sixTexture} attach="material-5" />
-//     </mesh>
-//   );
-// }
-
-// export default function R3fDemo() {
-//   const [targetRotation, setTargetRotation] = useState({ x: 0, y: 0 });
-
-//   const handleMusicClick = () => {
-//     setTargetRotation({ x: 0, y: -Math.PI / 2 });
-//   };
-//   const handleSportsClick = () => {
-//     setTargetRotation({ x: 0, y: Math.PI / 2 });
-//   };
-
-//   return (
-//     <>
-//       <button className="d" onClick={handleMusicClick}>
-//         Music
-//       </button>
-//       <button className="d" onClick={handleSportsClick}>
-//         Sports
-//       </button>
-
-//       <Canvas camera={{ position: defaultPos, fov: 50 }}>
-//         <ambientLight intensity={1} />
-//         <directionalLight position={[-2, 2, 3]} />
-//         <Cube
-//           position={[0, 0, 0]}
-//           size={[2.5, 2.5, 2.5]}
-//           targetRotation={targetRotation}
-//         />
-//         <TrackballControls rotateSpeed={3} noZoom={true} noPan={true} />
-//       </Canvas>
-//     </>
-//   );
-// }
-
-//usefram rotates the cube, meaning i will have to do something there, either find a way to change the camera to point to the specific side of cube or rotate the cube to the side where you want to camera to see it.
-//I think i will have to use usethree camera 
-import { Canvas, useFrame, useLoader } from "@react-three/fiber";
-import { TextureLoader } from "three";
-import { useRef, useState } from "react";
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame,useLoader } from "@react-three/fiber";
 import { TrackballControls } from "@react-three/drei";
+import * as THREE from "three";
+import { TextureLoader } from "three";
+
 
 const defaultPos = [3, 3, 7];
+const cubeSize = 2.5;
+const halfSize = cubeSize / 2;
 
-function Cube({ position, size, targetRotation }) {
+function Cube({ targetQuaternion, targetPosition }) {
   const meshRef = useRef();
+
   const textures = useLoader(TextureLoader, [
     "/image/2.jpg",
     "/image/1.jpeg",
@@ -92,60 +22,96 @@ function Cube({ position, size, targetRotation }) {
   ]);
 
   useFrame(() => {
-    if (!meshRef.current) return;
-    meshRef.current.rotation.y +=
-      (targetRotation.y - meshRef.current.rotation.y) * 0.1;
-    meshRef.current.rotation.x +=
-      (targetRotation.x - meshRef.current.rotation.x) * 0.1;
-      meshRef.current.rotation.z +=
-      (targetRotation.z - meshRef.current.rotation.z) * 0.1;
-
-
-
-
+    if (meshRef.current) {
+      meshRef.current.quaternion.slerp(targetQuaternion, 0.1);
+      meshRef.current.position.lerp(targetPosition, 0.1);
+    }
   });
 
   return (
-    <mesh position={position} ref={meshRef}>
-      <boxGeometry args={size} />
-      {textures.map((texture, index) => (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[cubeSize, cubeSize, cubeSize]} />
+
+      {textures.map((ele, index) => (
         <meshStandardMaterial
           key={index}
-          map={texture}
-          attach={`material-${index}`}
+          map={ele}
+            attach={`material-${index}`}
         />
       ))}
     </mesh>
   );
 }
 
-export default function R3fDemo() {
-  const [targetRotation, setTargetRotation] = useState({ x: 0, y: 0, z: 0 });
+export default function App() {
+  const [targetQuaternion, setTargetQuaternion] = useState(
+    new THREE.Quaternion()
+  );
+  const [targetPosition, setTargetPosition] = useState(
+    new THREE.Vector3(0, 0, 0)
+  );
+  const [camera, setCamera] = useState(null);
 
-  const handleMusicClick = () => {
-    setTargetRotation({ x: 0, y: -Math.PI / 2, z:0 });
+  const Q_music = new THREE.Quaternion().setFromAxisAngle(
+    new THREE.Vector3(1, 0, 0),
+    -Math.PI / 2
+  );
+  const Q_sports = new THREE.Quaternion().setFromAxisAngle(
+    new THREE.Vector3(1, 0, 0),
+    Math.PI / 2
+  );
+
+  const handleMusicHover = () => {
+    if (!camera) return;
+    const targetQuat = camera.quaternion.clone().multiply(Q_music);
+    let offset = new THREE.Vector3(0, halfSize, 0).applyQuaternion(targetQuat);
+    const targetPos = offset.multiplyScalar(-1);
+    setTargetQuaternion(targetQuat);
+    setTargetPosition(targetPos);
   };
 
-  const handleSportsClick = () => {
-    setTargetRotation({ x: 0, y: Math.PI / 2,z:0 });
+  const handleSportsHover = () => {
+    if (!camera) return;
+    const targetQuat = camera.quaternion.clone().multiply(Q_sports);
+    let offset = new THREE.Vector3(0, -halfSize, 0).applyQuaternion(targetQuat);
+    const targetPos = offset.multiplyScalar(-1);
+    setTargetQuaternion(targetQuat);
+    setTargetPosition(targetPos);
+  };
+
+  const resetTransform = () => {
+    setTargetQuaternion(new THREE.Quaternion());
+    setTargetPosition(new THREE.Vector3(0, 0, 0));
   };
 
   return (
     <>
-      <button className="d" onMouseEnter={handleMusicClick}>
+      <button
+        className="d"
+        onMouseEnter={handleMusicHover}
+        onMouseLeave={resetTransform}
+      >
         Music
       </button>
-      <button className="d" onMouseEnter={handleSportsClick}>
+      <button
+        className="d"
+        onMouseEnter={handleSportsHover}
+        onMouseLeave={resetTransform}
+      >
         Sports
       </button>
 
-      <Canvas camera={{ position: defaultPos, fov: 50 }}>
+      <Canvas
+        camera={{ position: defaultPos, fov: 50 }}
+        onCreated={({ camera }) => {
+          setCamera(camera);
+        }}
+      >
         <ambientLight intensity={1} />
         <directionalLight position={[-2, 2, 3]} />
         <Cube
-          position={[0, 0, 0]}
-          size={[2.5, 2.5, 2.5]}
-          targetRotation={targetRotation}
+          targetQuaternion={targetQuaternion}
+          targetPosition={targetPosition}
         />
         <TrackballControls rotateSpeed={1} noZoom noPan makeDefault />
       </Canvas>
